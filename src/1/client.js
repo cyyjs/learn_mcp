42cad1a86f84c34ad012c0e85b918d7500245ce7
cyyjs <cyyjs@cyyjs.top>
2025-06-09 14:19:32 +0800

    feat: client update

--- src/1/client.js
+++ src/1/client.js
@@ -2,11 +2,19 @@
  * @Author: cyy
  * @Date: 2025-06-06 11:56:29
  * @LastEditors: cyy
- * @LastEditTime: 2025-06-06 12:26:42
+ * @LastEditTime: 2025-06-09 14:18:32
  * @Description: 客户端调用示例
  */
 import { Client } from "@modelcontextprotocol/sdk/client/index.js";
 import { StdioClientTransport } from "@modelcontextprotocol/sdk/client/stdio.js";
+import { Anthropic } from "@anthropic-ai/sdk";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const anthropic = new Anthropic({
+  baseURL: process.env.OPENAI_BASE_URL,
+  apiKey: 'xx',
+});
 
 const transport = new StdioClientTransport({
   command: "node",
@@ -30,5 +38,28 @@ const result = await client.callTool({
     b: 22
   }
 });
-
 console.log(result)
+
+
+const toolsResult = await client.listTools();
+console.log(JSON.stringify(toolsResult, null, 2))
+
+const response = await anthropic.messages.create({
+  messages: [{ role: 'user', content: '8加9再乘5等于多少？' }],
+  model: 'Qwen-72B-Chat',
+  max_tokens: 1000,
+  tools: toolsResult.tools,
+})
+console.log(response)
+for (const content of response.content) {
+  console.log(content)
+  // if (content.type === 'tool_use') {
+  //   const r = await client.callTool({
+  //     name: content.name,
+  //     arguments: content.input
+  //   })
+  //   console.log(r)
+  // } else {
+  //   console.log(content)
+  // }
+}
